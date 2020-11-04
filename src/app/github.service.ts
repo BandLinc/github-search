04dfd83ee76c421093349e15e5568d076604779c
LincolnLidanya <lincolnlidanya@gmail.com>
2020-11-04 03:18:23 +0300

    Create github service components

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
+//Create github service
+
 @Injectable()
 export class GithubService {
   username: string;
